b192301211b0df27c8a83dfb7df571c3dbe1513b
JoshPell <462989817@qq.com>
2018-03-01 15:13:37 +0800

    first commit

---
+++ TJConvenientKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TJConvenientKit
+//
+//  Created by TanJian on 2018/3/1.
+//  Copyright © 2018年 Joshpell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TJConvenientKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TJConvenientKit
+//
+//  Created by TanJian on 2018/3/1.
+//  Copyright © 2018年 Joshpell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
